36f313e2b39e2aac9a813da36f197291528e2ca6
System Administrator <root@alexandria-seiers-macbook-pro.local>
2013-01-06 19:58:18 +0000

    Added Chevrons to side navigation bar

--- application/views/helpers/DisplayMenu.php
+++ application/views/helpers/DisplayMenu.php
@@ -54,48 +54,98 @@ class Zend_View_Helper_DisplayMenu extends Zend_View_Helper_Abstract
         </div>
     </div>
    
+    <!----------- TABLEAU DE BORD ------------>
     <div class="sidebar-nav">
         <a href="'.$dash.'" class="nav-header"><i class="icon-th-large"></i>Tableau de bord</a>
+    <!------------------END------------------->
         
             
-        
-        <a href="#mon-daral-menu" class="nav-header" data-toggle="collapse"><i class="icon-home"></i>Mon Daral</a>
+   <!--------------MON DARAL------------------>
+        <a href="#mon-daral-menu" class="nav-header" data-toggle="collapse"><i class="icon-home"></i>Mon Daral<i class="icon-chevron-down" id="chevron-mon-daral" style="margin-left:8.5em"></i></a>
         <ul id="mon-daral-menu" class="nav nav-list collapse">
+        
+        <script type="text/javascript">
+        $("a").click(function(){
+		  $(this).find("i#chevron-mon-daral").toggleClass("icon-chevron-down icon-chevron-up");
+		  
+		  });
+        </script>
            
-            <li> <a href="#eleveur-menu" class="nav-header " data-toggle="collapse" >Eleveurs</a></li>
+            <li> <a href="#eleveur-menu" class="nav-header " data-toggle="collapse" ><i class="icon-chevron-right" id="chevron-eleveurs"></i>Eleveurs</a></li>
               <ul id="eleveur-menu" class="nav nav-list collapse">
+              <script type="text/javascript">
+              $("a").click(function(){
+		      $(this).find("i#chevron-eleveurs").toggleClass("icon-chevron-right icon-chevron-down");
+		  
+		      });
+              </script>
                <li ><a href="'.$farmer.'">Liste</a></li>
                <li ><a href="'.$farmer.'/add">Inscription</a></li>
                <li ><a href="#">Recherche</a></li>
              </ul>
              
-            <li ><a href="#notification-menu" class="nav-header collapsed" data-toggle="collapse">Notifications</a></li>
+            <li ><a href="#notification-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-chevron-right" id="chevron-notifications"></i>Notifications</a></li>
              <ul id="notification-menu" class="nav nav-list collapse">
+             <script type="text/javascript">
+              $("a").click(function(){
+		      $(this).find("i#chevron-notifications").toggleClass("icon-chevron-right icon-chevron-down");
+		  
+		      });
+              </script>
                <li ><a href="'.$notification.'/create">Nouvelle Notification</a></li>
                <li><a href="'.$notification.'">Historique</a></li>
              </ul>
         
         </ul>
-        <a href="#media-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-film"></i>Mediatheque</a>
+        
+   <!---------------END------------------------>
+   
+   <!---------------MEDIATHEQUE---------------->
+        <a href="#media-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-film"></i>Mediatheque<i class="icon-chevron-down" id="chevron-media" style="margin-left: 7.2em"></i></a>
          <ul id="media-menu" class="nav nav-list collapse">
+         <script type="text/javascript">
+        $("a").click(function(){
+		  $(this).find("i#chevron-media").toggleClass("icon-chevron-down icon-chevron-up");
+		  
+		  });
+        </script>
                <li ><a href="'.$media.'">Liste des videos</a></li>
                <li><a href="#">Recherche</a></li>
          </ul>
-		
-         <a href="#stat-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-filter"></i>Statistiques</a>
+   <!----------------END----------------------->
+
+   <!---------------STATISTIQUES--------------->
+         <a href="#stat-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-filter"></i>Statistiques<i class="icon-chevron-down" id="chevron-stat" style="margin-left:7.7em"></i></a>
          <ul id="stat-menu" class="nav nav-list collapse">
+         <script type="text/javascript">
+        $("a").click(function(){
+		  $(this).find("i#chevron-stat").toggleClass("icon-chevron-down icon-chevron-up");
+		  
+		  });
+        </script>
                <li ><a href="#">Darals</a></li>
                <li><a href="#">Localites</a></li>
                <li><a href="#">Departements</a></li>
                <li><a href="#">Regions</a></li>
                <li><a href="#">Pays</a></li>
          </ul>
-        <a href="#contact-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-book"></i>Contacts</a>
+   <!----------------END----------------------->
+   
+   <!---------------CONTACTS------------------->
+        <a href="#contact-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-book"></i>Contacts<i class="icon-chevron-down" id="chevron-contacts" style="margin-left:8.8em"></i></a>
          <ul id="contact-menu" class="nav nav-list collapse">
+         <script type="text/javascript">
+        $("a").click(function(){
+		  $(this).find("i#chevron-contacts").toggleClass("icon-chevron-down icon-chevron-up");
+		  
+		  });
+        </script>
                <li ><a href="'.$veterinaire.'">Veterinaires</a></li>
                <li><a href="#">Administrateur</a></li>
                <li><a href="#">Autres Gerants</a></li>
          </ul>
+         
+   <!---------------END------------------------>
     </div>
             
             	
