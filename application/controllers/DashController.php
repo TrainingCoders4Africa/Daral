d3c622d3fc5d31a34a32b38f34ffebc0bf26b8d9
System Administrator <root@alexandria-seiers-macbook-pro.local>
2013-01-15 01:59:20 +0000

    dashboard integration

--- application/controllers/DashController.php
+++ application/controllers/DashController.php
@@ -3,15 +3,44 @@
 class DashController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
+	public function init()
+	{
+		/* Initialize action controller here */
+		 
+	}
 
-    public function indexAction()
-    {
-        // action body
-    }
+	public function indexAction()
+	{
+		$this->getFrontController()->getRequest()->setParams($_GET);
+
+		// zsf = zodeken sort field, zso = zodeken sort order
+		$sortField = $this->_getParam('_sf', '');
+		$sortOrder = $this->_getParam('_so', '');
+		$pageNumber = $this->_getParam('page', 1);
+
+		$tableFarmer = new Application_Model_Farmer_DbTable();
+		$gridSelect = $tableFarmer->getDbSelectByParams(array('isactive_farmer'=>'1'), $sortField, $sortOrder);
+		$paginator = Zend_Paginator::factory($gridSelect);
+		$paginator->setItemCountPerPage(10)
+		->setCurrentPageNumber(1);
+
+		 
+
+
+		$this->view->assign(array(
+            'paginator' => $paginator,
+            'sortField' => $sortField,
+            'sortOrder' => $sortOrder,
+            'pageNumber' => $pageNumber,
+		 
+		));
+
+		foreach ($this->_getAllParams() as $paramName => $paramValue)
+		{
+			// prepend 'param' to avoid error of setting private/protected members
+			$this->view->assign('param' . $paramName, $paramValue);
+		}
+	}
 
 
 }
--- application/models/Daral/DbTable.php
+++ application/models/Daral/DbTable.php
@@ -10,4 +10,121 @@
 class Application_Model_Daral_DbTable extends Application_Model_Daral_DbTable_Abstract
 {    
 	//write custom functions here
+	public function getDashStats()
+	{
+		$return = array();
+	
+	
+	
+		$nbFarmer  = "select count(*) from farmer where isactive_farmer = 1";
+		$nbCheptel = "select count(*) from animal";
+		$nbNotific = "select count(*) from notification";
+		$nbNotifVol="select count(*) from notification n,typenotification t where n.type=t.id and t.libelle like '%vol%'";
+		$LastNotifVol="select t.description,n.id_localite from notification n,typenotification t where t.id=n.type order by n.id desc limit 1";
+		
+		
+		$stmt0 = $this->_db->query($nbFarmer);
+		$rows0 = $stmt0->fetchAll(Zend_Db::FETCH_NUM);
+		if (count($rows0) == 0) {
+			return array();
+		}
+		foreach ($rows0 as $row)
+		{
+			$return[0] = $row[0];
+			
+		}
+	
+		$stmt1 = $this->_db->query($nbCheptel);
+		$rows1 = $stmt1->fetchAll(Zend_Db::FETCH_NUM);
+		if (count($rows1) == 0) {
+			return array();
+		}
+		foreach ($rows1 as $row)
+		{
+			$return[1] = $row[0];
+		}
+		
+		$stmt2 = $this->_db->query($nbNotific);
+		$rows2 = $stmt2->fetchAll(Zend_Db::FETCH_NUM);
+		if (count($rows2) == 0) {
+			return array();
+		}
+		foreach ($rows2 as $row)
+		{
+			$return[2] = $row[0];
+		}
+		
+		$stmt3 = $this->_db->query($nbNotifVol);
+		$rows3 = $stmt3->fetchAll(Zend_Db::FETCH_NUM);
+		if (count($rows3) == 0) {
+			return array();
+		}
+		foreach ($rows3 as $row)
+		{
+			$return[3] = $row[0];
+		}
+	
+		$stmt4 = $this->_db->query($LastNotifVol);
+		$rows4 = $stmt4->fetchAll(Zend_Db::FETCH_NUM);
+		if (count($rows4) == 0) {
+			return array();
+		}
+		foreach ($rows4 as $row)
+		{
+			$return[4] = $row[0];
+			$return[5] = $row[1];
+		}
+	
+		return $return; 
+	}
+	
+	
+	
+	public function getLastFarmers()
+	{
+	  $return=array();
+	  $lastFarmers="select * from farmer order by rank_farmer desc limit 5 ";
+	  
+	  $stmt = $this->_db->query($lastFarmers);
+	  $rows = $stmt->fetchAll(Zend_Db::FETCH_NUM);
+	  if (count($rows) == 0) {
+	  	return array();
+	  }
+	  
+	  return $rows;
+	    
+	}
+	
+	public function getLastNotifications()
+	{
+		$return=array();
+		$lastNotifications="select * from notification order by id desc limit 3 ";
+		 
+		$stmt = $this->_db->query($lastNotifications);
+		$rows = $stmt->fetchAll(Zend_Db::FETCH_NUM);
+		if (count($rows) == 0) {
+			return array();
+		}
+		 
+		return $rows;
+		 
+	}
+	
+	public function getLastDarals()
+	{
+		$return=array();
+		$lastDarals="select * from daral order by id desc limit 3 ";
+			
+		$stmt = $this->_db->query($lastDarals);
+		$rows = $stmt->fetchAll(Zend_Db::FETCH_NUM);
+		if (count($rows) == 0) {
+			return array();
+		}
+			
+		return $rows;
+			
+	}
+	
+	
 }
+	
